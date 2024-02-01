0c1b1c9e77546aa2bc932466b85952b4688d980e
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2024-01-31 20:51:35 -0330

    services added

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -7,9 +7,9 @@ export function ExploreServices() {
         </h1>
       </div>
 
-      <div className="flex flex-col shadow-lg rounded-2xl m-6">
+      <div className="flex flex-col shadow-lg rounded-2xl m-6 mb-24 md:mb-32">
         <div className="flex flex-col items-center">
-          <h1 className="bg-darkBlue w-full text-white font-bold text-center text-3xl p-4 rounded-t-2xl">
+          <h1 className="bg-darkBlue w-full text-white font-bold text-center text-3xl p-4 rounded-t-2xl lg:text-4xl">
             Living Rooms
           </h1>
         </div>
@@ -28,6 +28,99 @@ export function ExploreServices() {
               <img className="w-3 h-3" src="./img/dot.png"></img>
               <p>Cleaning and disinfecting high traffic areas and surfaces.</p>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col shadow-lg rounded-2xl m-6 mb-24 md:mb-32">
+        <div className="flex flex-col items-center">
+          <h1 className="bg-darkBlue w-full text-white font-bold text-center text-3xl p-4 rounded-t-2xl lg:text-4xl">
+            Bedrooms
+          </h1>
+        </div>
+        <div className="flex flex-col md:flex-row">
+          <img className="w-65 md:w-96" src="./img/bedroom.png"></img>
+          <div className="flex flex-col rounded-b-2xl md:text-xl lg:text-3xl gap-2 md:gap-6 md:pl-10 p-4">
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Dusing surfaces, windows, blinds, and curtains.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Vaccuming & mopping floors.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Cleaning and disinfecting high traffic areas and surfaces.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Changing bedding.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>
+                Laundry: We provide clean bedding set for our regular customers.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col shadow-lg rounded-2xl m-6 mb-24 md:mb-32">
+        <div className="flex flex-col items-center">
+          <h1 className="bg-darkBlue w-full text-white font-bold text-center text-3xl p-4 rounded-t-2xl lg:text-4xl">
+            Bathrooms
+          </h1>
+        </div>
+        <div className="flex flex-col md:flex-row">
+          <img className="w-65 md:w-96" src="./img/bathroom.png"></img>
+          <div className="flex flex-col rounded-b-2xl md:text-xl lg:text-3xl gap-2 md:gap-6 md:pl-10 p-4">
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Cleaning and disinfecting mirrors.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Cleaning and disinfecting toilet seat, shower, bathtubs.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Cleaning and disinfecting high traffic areas and surfaces.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Cleaning floors.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>
+                Laundry: We provide clean towels for our regular customers.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col shadow-lg rounded-2xl m-6 mb-24 md:mb-32">
+        <div className="flex flex-col items-center">
+          <h1 className="bg-darkBlue w-full text-white font-bold text-center text-3xl p-4 rounded-t-2xl lg:text-4xl">
+            Kitchens
+          </h1>
+        </div>
+        <div className="flex flex-col md:flex-row">
+          <img className="w-65 md:w-96" src="./img/kitchen.png"></img>
+          <div className="flex flex-col rounded-b-2xl md:text-xl lg:text-3xl gap-2 md:gap-6 md:pl-10 p-4">
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Vaccuming & mopping floors.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Washing dishes.</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <img className="w-3 h-3" src="./img/dot.png"></img>
+              <p>Cleaning and disinfecting surfaces.</p>
+            </div>
+          
           </div>
         </div>
       </div>
